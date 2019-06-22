04db8f0f9b4c154b81e108a23c97f412e1f8525b
PlazMa <32039975+Bowlingtoolkit@users.noreply.github.com>
2019-06-22 18:36:55 +0300

    Update raiding.js

--- Tools/raiding.js
+++ Tools/raiding.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const fs = require("fs");
 const cache = JSON.parse(fs.readFileSync("./caches.json", "utf8"))
 
 client.on("ready", () => {
