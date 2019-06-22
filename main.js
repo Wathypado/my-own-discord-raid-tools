8b65d1d7bc92b78812214c6412ff107c8c7f2267
PlazMa <32039975+Bowlingtoolkit@users.noreply.github.com>
2019-06-22 17:06:21 +0300

    Update main.js

--- main.js
+++ main.js
@@ -166,7 +166,7 @@ invite: link
 }
 
 
-if(option !== "1" && option !== "2" && option !== "3" && option !== "4" && option !== "5" && option !== "6" && option !== "7") {
+if(option !== "1" && option !== "2" && option !== "3" && option !== "4" && option !== "5" && option !== "6") {
 print("Nope, The Option Should Be 1-7 Only !")
 }
 
